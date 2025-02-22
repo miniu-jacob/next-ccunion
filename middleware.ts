6bc76ac1615f34edd3240b70f9b90648babb15bf
Jacob CHUNG <miniu.media@gmail.com>
2025-02-22 16:53:07 +0700

    03-10. update middleware for updating language priority

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { routing } from "./i18n/routing";
 import NextAuth from "next-auth";
 import authConfig from "./lib/auth.config";
 import { clog } from "./lib/jlogger";
+import { NextResponse } from "next/server";
 
 /*PUBLIC ROUTE (NO LOGIN REQUIRED)
  * =========================================
@@ -45,18 +46,55 @@ const { auth } = NextAuth(authConfig);
  * ========================================= */
 export default auth((req) => {
   // [Step A] next-intl 미들웨어 (rewrite/locale 설정 등) 먼저 실행
-  const response = intlMiddleware(req);
   const { pathname } = req.nextUrl;
-
-  clog.log("[middleware - Step A] pathname comes: ", pathname);
 
   /* URL PREFIX 추출
    * =============================
    * [Step A] URL에서 prefix 를 추출한다.
+   *   - locales: 언어 목록
+   *   - defaultLocale: 기본 언어
    *  ============================= */
+  const locales = routing.locales;
+  const defaultLocale = routing.defaultLocale;
+
+  // 1). URL prefix 추출 및 로깅
   const prefix = pathname.split("/")[1] || "";
 
-  // next-intl 미들웨어만 실행하고 바로 반환
+  clog.info("[middleware - Step A] pathname comes: ", pathname);
+  clog.info("[middleware - Step A] prefix extracted: ", prefix);
+
+  /* LANGUAGE SETTINGS
+   * =============================
+   * [Step B] 언어 설정
+   *   - currentLocale: URL에서 추출한 언어(prefix) -> 쿠키 -> 기본 언어 순서
+   *  ============================= */
+  const currentLocale = locales.includes(prefix) ? prefix : req.cookies.get("NEXT_LOCALE")?.value || defaultLocale;
+
+  clog.info("[middleware - Step B] currentLocale: ", currentLocale);
+
+  /* next-intl 미들웨어를 실행
+   * =============================
+   * [Step C] next-intl 미들웨어 실행
+   *   - currentLocale: URL에서 추출한 언어(prefix) -> 쿠키 -> 기본 언어 순서
+   *  ============================= */
+  const response = intlMiddleware(req);
+
+  /* COOKIE SETTINGS
+   * =============================
+   * [Step D] NEXT_LOCALE 쿠키 설정
+   *   - 조건에 따라 쿠키를 설정한다.
+   *   - a). NEXT_LOCALE 쿠키를 가져오지 못한 경우
+   *   - b). NEXT_LOCALE 쿠키가 있지만, 현재 언어(currentLocale)와 다른 경우
+   *  ============================= */
+  if (!req.cookies.get("NEXT_LOCALE") || req.cookies.get("NEXT_LOCALE")?.value !== currentLocale) {
+    response.cookies.set("NEXT_LOCALE", currentLocale, {
+      path: "/",
+      maxAge: 60 * 60 * 24 * 365, // 30 days
+      secure: process.env.NODE_ENV === "production", // VERCEL HTTPS support
+    });
+    clog.log("[middleware - Step D] NEXT_LOCALE cookie set: ", currentLocale);
+  }
+
   return response;
 });
 
